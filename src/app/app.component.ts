e839eeb278d2d0fabf587c4d635765a144cf0ece
goktug5000 <goktug5000@gmail.com>
2023-10-11 14:50:56 +0300

    display All

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { AfterViewInit, Component, OnInit } from '@angular/core';
-import { Model } from './model';
+import { Model, ToDoItem } from './model';
 
 @Component({
   selector: 'app-root',
@@ -8,13 +8,26 @@ import { Model } from './model';
 })
 export class AppComponent{
   title = 'todoApp';
+  isDisplay=false;
 
   model = new Model();
   getName(){
     return this.model.user;
   }
   getItems(){
-    return this.model.itemss.filter((item: { action: any; })=> !item.action);
+    if(this.isDisplay){
+      return this.model.itemss;
+    }
+    else{
+      return this.model.itemss.filter((item: { action: any; })=> !item.action);
+    }
+
+  }
+
+  addItem(value:any){
+    if(value!=""){
+      this.model.itemss.push(new ToDoItem(value,false));
+    }
   }
 
 }
