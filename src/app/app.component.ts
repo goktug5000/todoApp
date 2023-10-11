c19a3f12fd244bcf6f1e5062c3fe71e547f2dc3a
goktug5000 <goktug5000@gmail.com>
2023-10-11 14:07:38 +0300

    For kurdum

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent{
   title = 'todoApp';
+  user = 'goktug';
+
+  itemss:any =  [
+    { desc: "kahvaltı", action: "no"},
+    { desc: "spor", action: "no" },
+    { desc: "yemek", action: "no" }
+  ];
+  
 }
